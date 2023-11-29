7169ffa7dae025f7ecaca25e0a0f3c36d2749e56
AndresJalife <ajalife@fi.uba.ar>
2023-11-28 22:02:55 -0300

    apetito de tina

--- src/components/proyects/proyects_components.js
+++ src/components/proyects/proyects_components.js
@@ -1,12 +1,14 @@
 import Solar from './Solar';
 import Documentales from './Documentales';
 import SpeedOfSound from "./SpeedOfSound";
+import ApetitoDeTina from "./ApetitoDeTina";
 import './proyectos.css';
 
 let project_data = {
     'Solar' : Solar,
     'Documentales': Documentales,
-    'SpeedOfSound': SpeedOfSound
+    'SpeedOfSound': SpeedOfSound,
+    'ApetitoDeTina': ApetitoDeTina
 
 }
 
