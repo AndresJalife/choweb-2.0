69cff1149247fff60cce6c085b07d5526d3252ec
Andres Jalife <andres.jalife@mercadolibre.com>
2025-04-24 15:06:33 -0300

    readd backend)?

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "choweb5ba14cf1": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "chowjuanstorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
